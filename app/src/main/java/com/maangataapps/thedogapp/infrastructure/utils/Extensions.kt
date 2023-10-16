527b5a7b516601ac7eff9a696ceb2eb2fec9c8fe
asdgvarela <asdgvarela@gmail.com>
2023-10-16 12:44:53 +0200

    Adds downloading and saving breeds info api, interactor and repository.

--- app/src/main/java/com/maangataapps/thedogapp/infrastructure/utils/Extensions.kt
+++ app/src/main/java/com/maangataapps/thedogapp/infrastructure/utils/Extensions.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.maangataapps.thedogapp.domain.model.details.BreedDetails
 import com.maangataapps.thedogapp.presentation.composables.RedSnackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -17,6 +18,10 @@ object Extensions {
 
     fun Boolean.ifTrue(actionIfTrue: () -> Unit) {
         if (this) actionIfTrue.invoke()
+    }
+
+    fun Boolean.ifFalse(actionIfTrue: () -> Unit) {
+        if (!this) actionIfTrue.invoke()
     }
 
     @Composable
@@ -28,6 +33,9 @@ object Extensions {
         }
     }
 
+    fun String?.orFillEmptyField(): String {
+        return if (this.isNullOrEmpty()) "-" else this
+    }
 
     fun String.showTextInSnackbar(scope: CoroutineScope, snackbarHostState: SnackbarHostState) {
         scope.launch {
@@ -65,5 +73,35 @@ object Extensions {
             }
         })
     }
-    
+
+    fun <T>NetworkUtils.checkConnectionAndDo(
+        connected: () -> T,
+        disconnected: () -> T
+    ): T {
+        return if (isOnline()) {
+            connected.invoke()
+        } else {
+            disconnected.invoke()
+        }
+    }
+
+    fun BreedDetails?.orEmpty(): BreedDetails {
+        return this
+            ?: BreedDetails(
+                id = 0,
+                name = "",
+                category = "",
+                origin = "",
+                temperament = ""
+            )
+    }
+
+    fun BreedDetails?.allFieldsEmpty(): Boolean {
+        return this?.id == null &&
+                this?.name.isNullOrEmpty() &&
+                this?.category.isNullOrEmpty() &&
+                this?.origin.isNullOrEmpty() &&
+                this?.temperament.isNullOrEmpty()
+    }
+
 }
